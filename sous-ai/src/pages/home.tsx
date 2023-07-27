cd4c892bf0c3d9dacbab86954cdc67a8ed43d39a
Yousef Macer <yousefmacer@gmail.com>
2023-07-27 17:58:03 -0400

    input fields

--- sous-ai/src/pages/home.tsx
+++ sous-ai/src/pages/home.tsx
@@ -1,14 +1,42 @@
-import { useState } from 'react'
+import React, { useState } from "react";
+import InputField from "../components/input";
 
+const App: React.FC = () => {
+	const [inputValue, setInputValue] = useState("");
 
-function HomePage() {
+	const handleInputChange = (value: string) => {
+		setInputValue(value);
+	};
 
+	return (
+		<div>
+			<InputField
+				label="First Name:"
+				value={inputValue}
+				onChange={handleInputChange}
+			/>
+			<InputField
+				label="Last Name:"
+				value={inputValue}
+				onChange={handleInputChange}
+			/>
+			<InputField
+				label="Username:"
+				value={inputValue}
+				onChange={handleInputChange}
+			/>
+			<InputField
+				label="Create a password:"
+				value={inputValue}
+				onChange={handleInputChange}
+			/>
+			<InputField
+				label="Confirm:"
+				value={inputValue}
+				onChange={handleInputChange}
+			/>
+		</div>
+	);
+};
 
-  return (
-    <>
-      <h1>The</h1>
-    </>
-  )
-}
-
-export default HomePage
+export default App;
