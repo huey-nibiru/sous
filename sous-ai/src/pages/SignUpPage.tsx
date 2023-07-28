c993c13f282d159e0bb39dc3a0305c7251d742db
Yousef Macer <yousefmacer@gmail.com>
2023-07-28 17:51:44 -0400

    Sign Up Page Align Form

--- sous-ai/src/pages/SignUpPage.tsx
+++ sous-ai/src/pages/SignUpPage.tsx
@@ -6,19 +6,32 @@ import FormComponent from "../components/SignUpForm";
 const App: React.FC = () => {
 	const containerStyle = {
 		display: "flex",
-		alignItems: "center", // Align the items vertically (change this as needed)
+		alignItems: "center",
+		//flexDirection: "column", // Align items in a column
+		textAlign: "center", // Center the h1 element horizontally
 	};
 
 	const imageStyle = {
-		width: "200px", // Adjust the width of the image as needed
+		width: "400px", // Adjust the width of the image as needed
 		//height: "200px", // Set the desired height
-		marginRight: "20px", // Add some space between the image and the form
+		marginRight: "100px", // Add some space between the image and the form
+	};
+
+	const headerContainer = {
+		textAlign: "center",
+		marginLeft: "auto",
+		marginTop: "10px",
+	};
+	const formColumn = {
+		flexDirection: "column", // Align form and header items in a column
 	};
 
 	return (
 		<>
-			<div className="wrapper" style={containerStyle}>
-				
+			{
+				// Align the clipart with the form horizontally
+			}
+			<div style={containerStyle}>
 				<div className="clipArtContainer">
 					<img
 						className="clipArt"
@@ -28,13 +41,18 @@ const App: React.FC = () => {
 					/>
 				</div>
 
-				<div className="headerContainer">
-					<h1>Sign Up</h1>
-					<h4>Let's get started!</h4>
-				</div>
+				{
+					//	Align the form with the header vertically
+				}
+				<div style={formColumn}>
+					<div style={headerContainer}>
+						<h1>Sign Up</h1>
+						<h4>Let's get started!</h4>
+					</div>
 
-				<div className="formContainer">
-					<FormComponent />
+					<div className="formContainer">
+						<FormComponent />
+					</div>
 				</div>
 			</div>
 		</>
