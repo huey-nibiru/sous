01273418e0cd3330a27a4ea1aa303152abd68ffe
Yousef Macer <yousefmacer@gmail.com>
2023-08-06 18:16:31 -0400

    routes

--- sous-ai/src/main.tsx
+++ sous-ai/src/main.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import SignUpPage from "./pages/Register.tsx";
 import LoginPage from "./pages/Login.tsx";
+import SignUpPage from "./pages/Register.tsx";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App.tsx";
 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<LoginPage />
+		<Router>
+			<App />
+		</Router>
 	</React.StrictMode>
 );
