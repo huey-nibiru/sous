d4b7c277e369a5805d29080a294672e0724d2809
Yousef Macer <yousefmacer@gmail.com>
2023-08-06 21:03:32 -0400

    Update Navbar.tsx

--- sous-ai/src/components/Navbar.tsx
+++ sous-ai/src/components/Navbar.tsx
@@ -3,9 +3,17 @@ import { Link } from "react-router-dom";
 function Navbar() {
 	return (
 		<nav>
-			<Link to="/">Home</Link>
-			<Link to="/register">Register</Link>
-			<Link to="/login">Login</Link>
+			<ul>
+				<li>
+					<Link to="/">Home</Link>
+				</li>
+				<li>
+					<Link to="/register">Register</Link>
+				</li>
+				<li>
+					<Link to="/login">Login</Link>
+				</li>
+			</ul>
 		</nav>
 	);
 }
