1d7baf7ec089b9e26fa14dbde875e17916e3f93f
Yousef Macer <yousefmacer@gmail.com>
2023-09-11 19:07:58 -0400

    Carousel Circular Component

--- sous-ai/src/pages/Home.tsx
+++ sous-ai/src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import Header from "../components/Header";
 import ContentRow from "../components/ContentRow";
 import CircleComponent from "../components/CircleContent";
+import "../assets/touchscreen.png";
 
 export default function () {
 	return (
@@ -13,9 +14,10 @@ export default function () {
 			</div>
 			<div>
 				<CircleComponent
-					imageUrl="your-image-url.jpg"
-					heading="Circle Heading"
-					paragraph="This is a paragraph inside the circle."
+					imageUrl="file:///var/folders/fm/gf09nxpx12n_7pcjsnlx5yt00000gn/T/TemporaryItems/NSIRD_screencaptureui_WUHnav/Screen%20Shot%202023-09-11%20at%206.52.05%20PM.png"
+					heading="1. Personalize Your Profile"
+					paragraph="Tell us about your dietary preferences, restrictions, and goals. 
+					Whether you're vegan, gluten-free, or aiming to lose weight, we've got you covered."
 				/>
 			</div>
 		</>
