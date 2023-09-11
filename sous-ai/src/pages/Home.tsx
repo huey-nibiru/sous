6ed55ac07f8e898109349f4de5a11c778f3726f8
Yousef Macer <yousefmacer@gmail.com>
2023-09-11 00:35:12 -0400

    Header

--- sous-ai/src/pages/Home.tsx
+++ sous-ai/src/pages/Home.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import "./styles/home.css"
 import LoginButton from "../components/LoginButton";
 
 export default function Home() {
 	return (
 		<>
-			<div>SOUS</div>
+			<div className="header">
+				<h1>My Website Header</h1>
+				{/* Add navigation links or other content here */}
+			</div>
+
 			<div className="App">
 				<LoginButton />
 			</div>
