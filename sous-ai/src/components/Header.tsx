6b1263b25bac2669211a87cad315dcc030630060
Yousef Macer <yousefmacer@gmail.com>
2023-09-11 00:40:17 -0400

    header

---
+++ sous-ai/src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles/header.css";
+import LoginButton from "../components/LoginButton";
+
+export default function () {
+	return (
+		<>
+			<div className="header">
+				<h1>SOUS</h1>
+				{/* Add navigation links or other content here */}
+			</div>
+		</>
+	);
+}
--- sous-ai/src/pages/Home.tsx
+++ sous-ai/src/pages/Home.tsx
@@ -1,17 +1,11 @@
 import React from "react";
-import "./styles/home.css";
-import LoginButton from "../components/LoginButton";
+import Header from "../components/Header";
 
-export default function Home() {
+export default function () {
 	return (
 		<>
-			<div className="header">
-				<h1>SOUS</h1>
-				{/* Add navigation links or other content here */}
-			</div>
-
-			<div className="App">
-				<LoginButton />
+			<div>
+				<Header />
 			</div>
 		</>
 	);
