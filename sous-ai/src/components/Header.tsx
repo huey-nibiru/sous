89edfad3c5efa75bc4feed2b5361b6b0800e0ff0
Yousef Macer <yousefmacer@gmail.com>
2023-09-11 00:48:30 -0400

    Title

--- sous-ai/src/components/Header.tsx
+++ sous-ai/src/components/Header.tsx
@@ -6,7 +6,7 @@ export default function () {
 	return (
 		<>
 			<div className="header">
-				<h1>SOUS</h1>
+				<h1>Sous</h1>
 				{/* Add navigation links or other content here */}
 			</div>
 		</>
