a7e4b0c217772a58fec65897ff68c894bceec24b
Yousef Macer <yousefmacer@gmail.com>
2023-09-11 00:29:22 -0400

    Title

--- sous-ai/src/components/LoginButton.tsx
+++ sous-ai/src/components/LoginButton.tsx
@@ -1,25 +1,25 @@
-import React, { useState } from 'react';
-import './LoginSignupButton.css'; // You can style the button in a separate CSS file
+import React, { useState } from "react";
+import "./LoginButton.css"; // You can style the button in a separate CSS file
 
 interface LoginSignupButtonProps {
-  // You can define any additional props you need here
+	// You can define any additional props you need here
 }
 
 const LoginSignupButton: React.FC<LoginSignupButtonProps> = () => {
-  const [isGlowing, setIsGlowing] = useState(false);
+	const [isGlowing, setIsGlowing] = useState(false);
 
-  const handleButtonClick = () => {
-    setIsGlowing(!isGlowing);
-  };
+	const handleButtonClick = () => {
+		setIsGlowing(!isGlowing);
+	};
 
-  return (
-    <button
-      className={`login-signup-button ${isGlowing ? 'glow' : ''}`}
-      onClick={handleButtonClick}
-    >
-      {isGlowing ? 'Sign Up' : 'Login'}
-    </button>
-  );
+	return (
+		<button
+			className={`login-signup-button ${isGlowing ? "glow" : ""}`}
+			onClick={handleButtonClick}
+		>
+			{isGlowing ? "Sign Up" : "Login"}
+		</button>
+	);
 };
 
 export default LoginSignupButton;
--- sous-ai/src/pages/Home.tsx
+++ sous-ai/src/pages/Home.tsx
@@ -1,15 +1,13 @@
 import React from "react";
-import LoginButton from '../components/LoginButton'
-
+import LoginButton from "../components/LoginButton";
 
 export default function Home() {
 	return (
 		<>
-			 <div className="App">
-            <LoginButton />
-
-            {/* Rest of your app content */}
-        </div>
+			<div>SOUS</div>
+			<div className="App">
+				<LoginButton />
+			</div>
 		</>
 	);
 }
