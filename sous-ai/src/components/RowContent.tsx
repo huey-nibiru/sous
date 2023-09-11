5cff204367f8dca4028a4d9a5327dc1715ecb8d4
Yousef Macer <yousefmacer@gmail.com>
2023-09-11 19:28:26 -0400

    Refactor

--- sous-ai/src/components/RowContent.tsx
+++ sous-ai/src/components/RowContent.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./styles/ContentRow.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
--- sous-ai/src/pages/Home.tsx
+++ sous-ai/src/pages/Home.tsx
@@ -1,7 +1,6 @@
 import Header from "../components/Header";
 import ContentRow from "../components/RowContent";
 import CircleComponent from "../components/CircleContent";
-import "../assets/touchscreen.png";
 
 export default function () {
 	return (
@@ -14,7 +13,7 @@ export default function () {
 			</div>
 			<div>
 				<CircleComponent
-					imageUrl="file:///var/folders/fm/gf09nxpx12n_7pcjsnlx5yt00000gn/T/TemporaryItems/NSIRD_screencaptureui_WUHnav/Screen%20Shot%202023-09-11%20at%206.52.05%20PM.png"
+					imageUrl=""
 					heading="1. Personalize Your Profile"
 					paragraph="Tell us about your dietary preferences, restrictions, and goals. 
 					Whether you're vegan, gluten-free, or aiming to lose weight, we've got you covered!"
