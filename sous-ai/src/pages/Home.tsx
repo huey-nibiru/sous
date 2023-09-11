67f6c86bbaa075f7bf51c1ee4e03083209fea559
Yousef Macer <yousefmacer@gmail.com>
2023-09-11 00:36:30 -0400

    Title

--- sous-ai/src/pages/Home.tsx
+++ sous-ai/src/pages/Home.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import "./styles/home.css"
+import "./styles/home.css";
 import LoginButton from "../components/LoginButton";
 
 export default function Home() {
 	return (
 		<>
 			<div className="header">
-				<h1>My Website Header</h1>
+				<h1>SOUS</h1>
 				{/* Add navigation links or other content here */}
 			</div>
 
