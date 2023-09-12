0f602fb94cab5f73d1e854e1ecbb97af188c9bc4
Yousef Macer <yousefmacer@gmail.com>
2023-09-12 16:36:47 -0400

    Update banner

--- sous-ai/src/pages/Home.tsx
+++ sous-ai/src/pages/Home.tsx
@@ -5,7 +5,7 @@ import Carousel from "../components/Carousel";
 
 const circleData = [
 	{
-		imageUrl: "image1.jpg",
+		imageUrl: "image1.png",
 		heading: "1. Personalize Your Profile",
 		paragraph:
 			"Tell us about your dietary preferences, restrictions, and goals. Whether you're vegan, gluten-free, or aiming to lose weight, we've got you covered!",
