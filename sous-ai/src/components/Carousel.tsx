3f167085b2d497ef511c91d627bec7832e2a8a5f
Yousef Macer <yousefmacer@gmail.com>
2023-09-14 00:37:02 -0400

    carousel images

--- sous-ai/src/components/Carousel.tsx
+++ sous-ai/src/components/Carousel.tsx
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from "react";
 import CircleComponent from "./CircleContent"; // Import your CircleComponent and its props interface
 import "./styles/Carousel.css"; // You can style the carousel component with CSS
+import touchscreen from "../assets/touchscreen.png";
+import dinner_table from "../assets/dinner_table.png";
+import basket from "../assets/basket.png";
+import planner from "../assets/planner.png";
+import cooking from "../assets/touchscreen.png";
 
 interface CarouselProps {
 	circleData: [];
@@ -8,31 +13,32 @@ interface CarouselProps {
 }
 const circleData = [
 	{
-		imageUrl: "image1.png",
+		imageUrl: touchscreen,
+		img_alt: "touchscreen",
 		heading: "1. Personalize Your Profile",
 		paragraph:
 			"Tell us about your dietary preferences, restrictions, and goals. Whether you're vegan, gluten-free, or aiming to lose weight, we've got you covered!",
 	},
 	{
-		imageUrl: "image2.jpg",
+		imageUrl: dinner_table,
 		heading: "2. Explore Your Meal Options",
 		paragraph:
 			"Once your profile is set, choose from a world of culinary possibilities. Browse through your personalized meal options for breakfast, lunch, dinner, and snacks.",
 	},
 	{
-		imageUrl: "image3.jpg",
+		imageUrl: planner,
 		heading: "3. Plan",
 		paragraph:
 			"When you're ready to hit the grocery store, our app generates a shopping list tailored to your meal plan. No more guesswork – shop with confidence!",
 	},
 	{
-		imageUrl: "image4.jpg",
+		imageUrl: cooking,
 		heading: "4. Cook & Enjoy",
 		paragraph:
 			"Enjoy the flavors of your custom-made dishes. Nourish your body while indulging in meals that align with your dietary needs.",
 	},
 	{
-		imageUrl: "image5.jpg",
+		imageUrl: basket,
 		heading: "5. Make Smart Shopping Lists",
 		paragraph:
 			"When you're ready to hit the grocery store, our app generates a shopping list tailored to your meal plan. No more guesswork – shop with confidence!",
@@ -61,6 +67,7 @@ const Carousel: React.FC<CarouselProps> = ({ circleData, interval }) => {
 				>
 					<CircleComponent
 						imageUrl={circle.imageUrl}
+						img_alt={circle.img_alt}
 						heading={circle.heading}
 						paragraph={circle.paragraph}
 					/>
--- sous-ai/src/components/CircleContent.tsx
+++ sous-ai/src/components/CircleContent.tsx
@@ -3,19 +3,21 @@ import "./styles/CircleContent.css"; // You can style the circle component with 
 
 interface CircleComponentProps {
 	imageUrl: string;
+	img_alt: string;
 	heading: string;
 	paragraph: string;
 }
 
 const CircleComponent: React.FC<CircleComponentProps> = ({
 	imageUrl,
+	img_alt,
 	heading,
 	paragraph,
 }) => {
 	return (
 		<div className="circle-container">
 			<div className="red-circle">
-				<img src={imageUrl} alt="Image" className="circle-image" />
+				<img src={imageUrl} alt={img_alt} className="circle-image" />
 				<h2 className="circle-heading">{heading}</h2>
 				<p className="circle-paragraph">{paragraph}</p>
 			</div>
