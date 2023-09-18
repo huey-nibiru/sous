278f7a34104ad81cc6864edf7c5b2914612729f0
Yousef Macer <yousefmacer@gmail.com>
2023-09-18 00:02:14 -0400

    add Login

--- sous-ai/src/App.tsx
+++ sous-ai/src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.tsx";
+import Login from "./pages/Login.tsx";
 
 function App() {
 	return (
 		<>
 			<Routes>
 				<Route path="/" element={<Home />} />
+				<Route path="/login" element={<Login />} />
 			</Routes>
 		</>
 	);
---
+++ sous-ai/src/pages/Login.tsx
@@ -0,0 +1,17 @@
+import Header from "../components/Header";
+import "./styles/Footer.css";
+
+export default function () {
+	return (
+		<>
+			<Header />
+
+
+
+
+			<div className="footer-pad">
+				<footer className="footer">Sous</footer>
+			</div>
+		</>
+	);
+}
