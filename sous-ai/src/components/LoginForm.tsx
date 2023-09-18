b0990adc900a611d86f120a22581b8bb6c325859
Yousef Macer <yousefmacer@gmail.com>
2023-09-18 16:15:24 -0400

    Login Page Update

--- sous-ai/src/components/LoginForm.tsx
+++ sous-ai/src/components/LoginForm.tsx
@@ -3,22 +3,24 @@ import "./styles/LoginForm.css";
 const LoginComponent: React.FC = () => {
 	return (
 		<div className="white-half-container">
-			<h1>Login</h1>
+			<h1>Log In</h1>
 			<form className="form-container">
 				<div className="input-group">
 					<h2>Email</h2>
-					<input type="text" placeholder="Enter your email" />
+					<input type="text" />
 				</div>
 				<div className="input-group">
 					<h2>Password</h2>
-					<input type="password" placeholder="Enter your password" />
+					<input type="password" />
 				</div>
 				<button className="log-in-button">Login</button>
 			</form>
-			<p className="forgot-password">
+			<p className="footer-link">
 				<a href="#">Forgot your password?</a>
 			</p>
-			<button className="sign-up-button">Sign Up</button>
+			<p className="footer-link">
+				<a href="#">Sign Up</a>
+			</p>
 		</div>
 	);
 };
