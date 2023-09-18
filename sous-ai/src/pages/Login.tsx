39bc69b5a763234dc544d92cf40671f9823a505a
Yousef Macer <yousefmacer@gmail.com>
2023-09-18 15:49:15 -0400

    Login Page Left side

--- sous-ai/src/pages/Login.tsx
+++ sous-ai/src/pages/Login.tsx
@@ -1,6 +1,9 @@
 import LoginComponent from "../components/LoginForm";
 import "./styles.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import LoginClipart from "../assets/LoginClipart.png";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
 
 export default function () {
 	return (
@@ -8,6 +11,11 @@ export default function () {
 			<body className="login-body">
 				<div className="centered-container">
 					<img src={LoginClipart} alt="Centered Image" />
+					<div className="icon-container">
+						<FontAwesomeIcon icon={faHouse} size="5x" />
+						<h1>Sous</h1>
+						<p>All your meal plans, in one place.</p>
+					</div>
 				</div>
 				<div className="main-content"></div>
 				<LoginComponent />
