8ec039f99ae2cd826f6202709866e1ee747888ab
Yousef Macer <yousefmacer@gmail.com>
2023-09-18 15:21:01 -0400

    update the login media

--- sous-ai/src/pages/Login.tsx
+++ sous-ai/src/pages/Login.tsx
@@ -1,10 +1,14 @@
 import LoginComponent from "../components/LoginForm";
 import "./styles.css";
+import LoginClipart from "../assets/LoginClipart.png";
 
 export default function () {
 	return (
 		<>
 			<body className="login-body">
+				<div className="centered-container">
+					<img src={LoginClipart} alt="Centered Image" />
+				</div>
 				<div className="main-content"></div>
 				<LoginComponent />
 			</body>
