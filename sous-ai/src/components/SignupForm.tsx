d2acb06f732e95a86fbcf1723b1fc87cfd510b27
Yousef Macer <yousefmacer@gmail.com>
2023-09-20 13:16:46 -0400

    Create Sign Up Page

---
+++ sous-ai/src/components/SignupForm.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./styles/LoginForm.css";
+const SignupComponent: React.FC = () => {
+	return (
+		<div className="white-half-container">
+			<h1>Sign Up</h1>
+			<form className="form-container">
+			<div className="input-group">
+					<h2>Name</h2>
+					<input type="text" />
+				</div>
+				<div className="input-group">
+					<h2>Email</h2>
+					<input type="text" />
+				</div>
+				<div className="input-group">
+					<h2>Password</h2>
+					<input type="password" />
+				</div>
+				<button className="log-in-button">Login</button>
+			</form>
+			<p className="footer-link">
+				<a href="#">Forgot your password?</a>
+			</p>
+			<p className="footer-link">
+				<a href="#">Log In</a>
+			</p>
+		</div>
+	);
+};
+
+export default SignupComponent;
---
+++ sous-ai/src/pages/Signup.tsx
@@ -0,0 +1,29 @@
+import SignupComponent from "../components/SignupForm";
+import "./styles.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import LoginClipart from "../assets/LoginClipart.png";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
+
+export default function () {
+	return (
+		<>
+			<body className="login-body">
+				<div className="centered-container">
+					<img src={LoginClipart} alt="Centered Image" />
+					<div className="icon-container">
+						<FontAwesomeIcon icon={faHouse} size="5x" />
+						<h1>Sous</h1>
+						<p>All your meal plans, in one place.</p>
+					</div>
+				</div>
+				<div className="main-content"></div>
+				<SignupComponent />
+			</body>
+
+			<div className="footer-pad">
+				<footer className="footer">Sous</footer>
+			</div>
+		</>
+	);
+}
