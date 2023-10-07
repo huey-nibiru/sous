e6c660976cdeb7535ec5a15855dc59466890ac78
Yousef Macer <yousefmacer@gmail.com>
2023-10-06 23:10:18 -0400

    Trial page

--- sous-ai/src/main.tsx
+++ sous-ai/src/main.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import LoginPage from "./pages/Login.tsx";
-import SignUpPage from "./pages/Register.tsx";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App.tsx";
-
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
