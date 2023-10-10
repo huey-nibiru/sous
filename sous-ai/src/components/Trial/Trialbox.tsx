a03fb77c6af7a8f4ca4bc11dbcd69eed6a32eee8
Yousef Macer <yousefmacer@gmail.com>
2023-10-09 21:41:46 -0400

    Trial Page

--- sous-ai/src/components/Trial/Trialbox.tsx
+++ sous-ai/src/components/Trial/Trialbox.tsx
@@ -1,18 +1,18 @@
 import React from "react";
-import "./Trial.css";
+import "./Trialbox.css";
 import salmon from "../../assets/salmon.png";
+import { useNavigate } from "react-router-dom";
 
 interface RedBoxWithImageProps {
 	imageUrl: string;
 	linkUrl: string;
 }
 
-const RedBoxWithImage: React.FC<RedBoxWithImageProps> = ({
-	imageUrl,
-	linkUrl,
-}) => {
-	const redirectToLink = () => {
-		window.location.href = linkUrl;
+const RedBoxWithImage: React.FC<RedBoxWithImageProps> = ({}) => {
+	const navigate = useNavigate();
+	const handleLoginClick = () => {
+		// You can navigate to the login screen or any other desired location
+		navigate("/trial"); // Replace '/login' with your actual login route
 	};
 
 	return (
@@ -20,7 +20,7 @@ const RedBoxWithImage: React.FC<RedBoxWithImageProps> = ({
 			{" "}
 			{/* Use the CSS class for styling */}
 			<img src={salmon} alt="Image" />
-			<button onClick={redirectToLink}>Create Your First Meal!</button>
+			<button onClick={handleLoginClick}>Create Your First Meal!</button>
 		</div>
 	);
 };
