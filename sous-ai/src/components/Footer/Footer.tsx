4df8d5a2254cb168c483c6ab08735947109e4d6b
Yousef Macer <yousefmacer@gmail.com>
2024-01-31 17:08:58 -0500

    header update

--- sous-ai/src/components/Footer/Footer.tsx
+++ sous-ai/src/components/Footer/Footer.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import "./styles/Footer.css";
 
+
 const Footer: React.FC = () => {
 	return <footer>Sous</footer>;
 };
--- sous-ai/src/components/Header/Header.tsx
+++ sous-ai/src/components/Header/Header.tsx
@@ -2,6 +2,7 @@ import "./Header.css";
 import LoginButton from "../Login/LoginButton";
 import SignUpButton from "../SignUp/SignUpButton";
 
+// Contains the header components of login and signup buttons
 export default function () {
 	return (
 		<>
